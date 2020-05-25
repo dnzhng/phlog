3c4c13d304511af6b16a9f6b84b23e6bb5c57e6a
Daniel Zhang <daniel.zhang@teespring.com>
2020-05-25 19:13:25 -0400

    add styled components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,7 @@ module.exports = {
         siteUrl: `https://phlog.com`,
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
